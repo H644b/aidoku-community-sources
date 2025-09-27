07734ea8c2d7c6f16c6caa30bb02be481f7c70fe
H644b <69388525+H644b@users.noreply.github.com>
2025-09-26 20:15:18 -0400

    Update build script to target WebAssembly

--- src/rust/en.mangafire/build.sh
+++ src/rust/en.mangafire/build.sh
@@ -1,7 +1,6 @@
-rm -f package.aix
-cargo +nightly build --release
+cargo +nightly build --release --target wasm32-unknown-unknown
 mkdir -p target/wasm32-unknown-unknown/release/Payload
-cp -av res/* target/wasm32-unknown-unknown/release/Payload
+cp res/* target/wasm32-unknown-unknown/release/Payload
 cp target/wasm32-unknown-unknown/release/*.wasm target/wasm32-unknown-unknown/release/Payload/main.wasm
 cd target/wasm32-unknown-unknown/release ; zip -r package.aix Payload
 mv package.aix ../../../package.aix

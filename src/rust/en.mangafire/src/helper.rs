0b5777ce915fdf947f175f135a1561cde81c5795
H644b <69388525+H644b@users.noreply.github.com>
2025-09-26 20:51:05 -0400

    Just worrkkk :sob:

--- src/rust/en.mangafire/src/helper.rs
+++ src/rust/en.mangafire/src/helper.rs
@@ -1,14 +1,32 @@
 #![no_std]
 extern crate alloc;
 
-use alloc::string::String;
+use alloc::{string::String, vec::Vec};
 use aidoku::imports::net::{HttpMethod, Request};
-use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
+use aidoku::prelude::encode_uri_component;
+use aidoku::prelude::{Manga, Chapter, Page};
 
 pub fn get(url: &str) -> String {
     Request::new(url, HttpMethod::Get).html().expect("Request failed").read()
 }
 
 pub fn urlencode(input: &str) -> String {
-    utf8_percent_encode(input, NON_ALPHANUMERIC).to_string()
+    encode_uri_component(input)
+}
+
+// ----- Parser stubs -----
+pub fn parse_manga_list(_html: String) -> Result<Vec<Manga>> {
+    Ok(Vec::new())
+}
+
+pub fn parse_manga_details(_html: String, _id: String) -> Result<Manga> {
+    Ok(Manga::new())
+}
+
+pub fn parse_chapter_list(_html: String) -> Result<Vec<Chapter>> {
+    Ok(Vec::new())
+}
+
+pub fn parse_page_list(_html: String) -> Result<Vec<Page>> {
+    Ok(Vec::new())
 }
--- src/rust/en.mangafire/src/lib.rs
+++ src/rust/en.mangafire/src/lib.rs
@@ -1,14 +1,11 @@
 #![no_std]
 extern crate alloc;
 
-use alloc::string::String;
-use alloc::vec::Vec;
-
+use alloc::{string::String, vec::Vec};
 use aidoku::{
-    error::{AidokuError, AidokuErrorKind},
     imports::net::{HttpMethod, Request},
     prelude::*,
-    Chapter, Filter, Manga, Page, ContentRating, Viewer,
+    Chapter, Filter, Manga, Page,
 };
 
 mod helper;
@@ -16,51 +13,48 @@ use helper::*;
 
 const BASE_URL: &str = "https://mangafire.to";
 
-pub fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult, AidokuError> {
+pub fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<Vec<Manga>> {
     let mut url = format!("{}/filter", BASE_URL);
     let mut queries: Vec<String> = Vec::new();
 
     for filter in filters {
-        match filter {
-            Filter::Select { key, value, .. } => {
-                let val = value.unwrap_or_default();
-                if val == "Any" { continue; }
-                match key.as_str() {
-                    "genre" => queries.push(format!("genre={}", val.to_lowercase().replace(' ', "-"))),
-                    "type" => queries.push(format!("type={}", val.to_lowercase())),
-                    "status" => queries.push(format!("status={}", val.to_lowercase().replace(' ', "-"))),
-                    "language" => {
-                        let code = match val.as_str() {
-                            "English" => "en",
-                            "French" => "fr",
-                            "Spanish" => "es",
-                            "Spanish (LATAM)" => "es-419",
-                            "Portuguese" => "pt",
-                            "Portuguese (BR)" => "pt-br",
-                            "Japanese" => "ja",
-                            _ => continue,
-                        };
-                        queries.push(format!("lang={}", code));
-                    }
-                    "year" => if val != "Any" { queries.push(format!("year={}", val)) },
-                    "length" => if val.starts_with(">=") {
-                        let num = val.trim_start_matches(">=").trim();
-                        queries.push(format!("chapters=>{}", num));
-                    }
-                    "sort" => {
-                        let mapped = match val.as_str() {
-                            "Newest" => "new",
-                            "Updated" => "updated",
-                            "Added" => "added",
-                            "Random" => "random",
-                            _ => continue,
-                        };
-                        queries.push(format!("sort={}", mapped));
-                    }
-                    _ => {}
+        if let Filter::Select { key, value, .. } = filter {
+            let val = value.unwrap_or_default();
+            if val == "Any" { continue; }
+            match key.as_str() {
+                "genre" => queries.push(format!("genre={}", val.to_lowercase().replace(' ', "-"))),
+                "type" => queries.push(format!("type={}", val.to_lowercase())),
+                "status" => queries.push(format!("status={}", val.to_lowercase().replace(' ', "-"))),
+                "language" => {
+                    let code = match val.as_str() {
+                        "English" => "en",
+                        "French" => "fr",
+                        "Spanish" => "es",
+                        "Spanish (LATAM)" => "es-419",
+                        "Portuguese" => "pt",
+                        "Portuguese (BR)" => "pt-br",
+                        "Japanese" => "ja",
+                        _ => continue,
+                    };
+                    queries.push(format!("lang={}", code));
                 }
+                "year" => if val != "Any" { queries.push(format!("year={}", val)) },
+                "length" => if val.starts_with(">=") {
+                    let num = val.trim_start_matches(">=").trim();
+                    queries.push(format!("chapters=>{}", num));
+                }
+                "sort" => {
+                    let mapped = match val.as_str() {
+                        "Newest" => "new",
+                        "Updated" => "updated",
+                        "Added" => "added",
+                        "Random" => "random",
+                        _ => continue,
+                    };
+                    queries.push(format!("sort={}", mapped));
+                }
+                _ => {}
             }
-            _ => {}
         }
     }
 
@@ -72,35 +66,29 @@ pub fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult
     parse_manga_list(html)
 }
 
-pub fn get_manga_details(id: String) -> Result<Manga, AidokuError> {
+pub fn get_manga_details(id: String) -> Result<Manga> {
     let url = format!("{}/manga/{}", BASE_URL, id);
     let html = Request::new(&url, HttpMethod::Get).html()?;
     parse_manga_details(html, id)
 }
 
-pub fn get_chapter_list(id: String) -> Result<Vec<Chapter>, AidokuError> {
+pub fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
     let url = format!("{}/manga/{}/chapters", BASE_URL, id);
     let html = Request::new(&url, HttpMethod::Get).html()?;
     parse_chapter_list(html)
 }
 
-pub fn get_page_list(_manga_id: String, chapter_id: String) -> Result<Vec<Page>, AidokuError> {
+pub fn get_page_list(_manga_id: String, chapter_id: String) -> Result<Vec<Page>> {
     let url = format!("{}/read/{}", BASE_URL, chapter_id);
     let html = Request::new(&url, HttpMethod::Get).html()?;
     parse_page_list(html)
 }
 
-pub fn handle_url(url: String) -> Result<DeepLink, AidokuError> {
+pub fn handle_url(url: String) -> Option<Manga> {
     if url.contains("/manga/") {
         let id = url.split("/manga/").nth(1).unwrap_or("").to_string();
-        let manga = get_manga_details(id.clone())?;
-        Ok(DeepLink {
-            manga: Some(manga),
-            chapter: None,
-        })
+        get_manga_details(id).ok()
     } else {
-        Err(AidokuError {
-            reason: AidokuErrorKind::Unimplemented,
-        })
+        None
     }
 }
